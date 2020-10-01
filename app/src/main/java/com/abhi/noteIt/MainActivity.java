23937f13073ebe95b26d051d71c981a6467b5941
Abhishek Patel <patelabhishek2007@gmail.com>
2020-10-01 05:53:52 +0530

    fixed #4 (Added Undo Feature)

--- app/src/main/java/com/abhi/noteIt/MainActivity.java
+++ app/src/main/java/com/abhi/noteIt/MainActivity.java
@@ -21,6 +21,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.noteIt.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -71,11 +72,26 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) {
+                final int adapterPosition = viewHolder.getAdapterPosition();
+                final Note mNote = adapter.getNoteAt(adapterPosition);
+                Snackbar snackbar = Snackbar
+                        .make(recyclerView, "Note Deleted", Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                int mAdapterPosition = viewHolder.getAdapterPosition();
+                                noteViewModel.insert(mNote);
+
+
+                            }
+                        });
+                snackbar.show();
                 noteViewModel.delete(adapter.getNoteAt(viewHolder.getAdapterPosition()));
-                Toast.makeText(getApplicationContext(), "Note Deleted!", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "Note Deleted!", Toast.LENGTH_SHORT).show();
             }
         }).attachToRecyclerView(recyclerView);
+
 
         adapter.setOnItemClickListener(new NoteAdapter.onItemClickListener() {
             @Override
