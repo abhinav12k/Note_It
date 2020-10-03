8611b3aa7fa769a570f4f628c96447810db9f180
Abhinav <abhi2000.rtk@gmail.com>
2020-10-03 12:36:10 +0530

    Revert "fixed #5 (Delete Feature Enhancement)"

--- app/src/main/java/com/abhi/noteIt/MainActivity.java
+++ app/src/main/java/com/abhi/noteIt/MainActivity.java
@@ -21,7 +21,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.noteIt.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,29 +64,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(new SwipeToDeleteCallback(this) {
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
             @Override
-            public void onSwiped(@NonNull final RecyclerView.ViewHolder viewHolder, int direction) {
-                final int adapterPosition = viewHolder.getAdapterPosition();
-                final Note mNote = adapter.getNoteAt(adapterPosition);
-                Snackbar snackbar = Snackbar
-                        .make(recyclerView, "Note Deleted", Snackbar.LENGTH_LONG)
-                        .setAction("UNDO", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                int mAdapterPosition = viewHolder.getAdapterPosition();
-                                noteViewModel.insert(mNote);
-
-
-                            }
-                        });
-                snackbar.show();
-                noteViewModel.delete(adapter.getNoteAt(viewHolder.getAdapterPosition()));
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
             }
-        });
-        itemTouchhelper.attachToRecyclerView(recyclerView);
 
-
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                noteViewModel.delete(adapter.getNoteAt(viewHolder.getAdapterPosition()));
+                Toast.makeText(getApplicationContext(), "Note Deleted!", Toast.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(recyclerView);
 
         adapter.setOnItemClickListener(new NoteAdapter.onItemClickListener() {
             @Override
